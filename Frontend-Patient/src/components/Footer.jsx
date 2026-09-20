65d5f0213b11e1a2ada44027474596ce50e9cd70
Emergin <ngangaephantus112@gmail.com>
2026-09-20 14:50:03 +0300

    changes

--- Frontend-Patient/src/components/Footer.jsx
+++ Frontend-Patient/src/components/Footer.jsx
@@ -25,51 +25,51 @@ const Footer = () => {
           </div>
           <div>
             <h4>Developed by</h4>
-            <p>Muhammad Fairooz</p>
+            <p>Nganga Ephantus</p>
             <div className="social-icons">
               <a
-                href="https://fairooz2150.github.io/Fairooz/"
+                href="https://github.com/Emergin"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <IoGlobeOutline />
               </a>
               <a
-                href="https://www.linkedin.com/in/muhammad-fairooz-0b1136268/?trk=public-profile-join-page"
+                href="https://www.linkedin.com/in/ephantus-ng-ang-a-621588231/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <RxLinkedinLogo />
               </a>
               <a
-                href="https://github.com/Fairooz2150"
+                href="https://github.com/Emergin"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaGithub />
               </a>
-              <a
+              {/* <a
                 href="https://www.instagram.com/marsh__mell_o/?igsh=MWhwZmI5NDRsMWoxMQ%3D%3D"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaInstagramSquare />
-              </a>
+              </a> */}
             </div>
           </div>
           <div>
             <h4>Contact</h4>
             <div>
               <FaPhone />
-              <span>999-999-9999</span>
+              <span>254-7999-9999</span>
             </div>
             <div>
               <MdEmail />
-              <span>lifecare@gmail.com</span>
+              <span>lifecare@gmal.com</span>
             </div>
             <div>
             <FaLocationDot/>
-            <span>Kerala, India</span>
+            <span>Nairobi, Kenya</span>
             </div>
           </div>
         </div>
